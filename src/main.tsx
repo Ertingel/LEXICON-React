7131cd146f513bbea901befc9da06ca1dbc5817f
Ertingel <152289948+Ertingel@users.noreply.github.com>
2024-02-23 13:58:49 +0100

    Github pages retry 6.

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,13 @@
 import React from "react"
-import { HashRouter } from "react-router-dom"
+import { BrowserRouter } from "react-router-dom"
 import ReactDOM from "react-dom/client"
 import App from "./App.tsx"
 import "./index.css"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
-		<h1>Testing</h1>
-		<HashRouter>
+		<BrowserRouter>
 			<App />
-		</HashRouter>
+		</BrowserRouter>
 	</React.StrictMode>
 )
